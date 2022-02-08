65dd0a5233797a7dcc9c7cb4f2cdefc2df3c191e
elaboranext <47183799+elaboranext@users.noreply.github.com>
2022-02-08 17:22:50 +0100

    Add files

---
+++ inc/superheroes.php
@@ -0,0 +1,26 @@
+<?php
+
+class SuperHero
+{
+    public $name;
+    public $powers;
+    public $image;
+}
+
+$superheroes = array();
+
+// Ecco una lista di supereroi che puoi utilizzare:
+
+// Nome: Flash
+// Poteri: Velocità supersonica, super riflessi, capacità di lanciare fulmini
+// Immagine: img/flash.jpg
+
+// Nome: Spider-Man
+// Poteri: Forza sovraumana, capacità di aderire alle pareti, agilità e riflessi eccezionali
+// Immagine: img/spiderman.jpg
+
+// Nome: Superman
+// Poteri: Forza e resistenza sovraumane, invulnerabilità, volo, vista a raggi X, super udito
+// Immagine: img/superman.png
+
+// Crea i tre supereroi e aggiungili all'array $superheroes
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include 'inc/superheroes.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superheroes cards</title>
+    
+    <!-- Per realizzare le card puoi aggiungere una libreria esterna o scrivere direttamente qui sotto il tuo codice CSS. -->
+
+</head>
+<body>
+    
+    <!-- Crea qui sotto le tre card utilizzando i dati contenuti nell'array $superheroes. -->
+    
+    <!-- Ora usa JavaScript per impostare come sfondo della pagina l'Astronomy Picture of the Day della NASA. -->
+     
+</body>
+</html>
